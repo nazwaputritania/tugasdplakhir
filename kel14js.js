61a3bd2181981398bf122fc25853cc751f4d7065
nazwaputritania <118706631+nazwaputritania@users.noreply.github.com>
2022-12-01 09:51:05 +0700

    Add files via upload

---
+++ kel14js.js
@@ -0,0 +1,8 @@
+// TUGAS KELOMPOK DPL
+// MEMBUAT KALKULATOR
+// Raissa Fadhillah X PPLG 2
+// Nazwa Putri Tania X PPLG 2
+
+const readline = require('readline-sync')
+console.log('SELAMAT DATANG DI KALKULATOR');
+var nilai1 = readline.questionInt('pilih fungsi: 1.Tambah, 2.kali 3.kurang 4.bagi = ')
