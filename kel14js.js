603959e5f3be06aa888b525cb588bb31dc2991e7
raissafadhilah <118705915+raissafadhilah@users.noreply.github.com>
2022-12-01 09:57:32 +0700

    Update kel14js.js

--- kel14js.js
+++ kel14js.js
@@ -6,3 +6,7 @@
 const readline = require('readline-sync')
 console.log('SELAMAT DATANG DI KALKULATOR');
 var nilai1 = readline.questionInt('pilih fungsi: 1.Tambah, 2.kali 3.kurang 4.bagi = ')
+if(nilai1 == 1){
+    var masukanNilai1 = readline.questionInt('masukan nilai 1= ')
+    var masukanNilai2 = readline.questionInt('masukan nilai 2= ')
+    console.log(Number(masukanNilai1)+(Number(masukanNilai2)))
